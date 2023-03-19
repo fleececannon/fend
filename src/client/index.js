07ab756ec019fb63112dcafb7ee171c9de367603
fleececannon <121724981+fleececannon@users.noreply.github.com>
2023-03-19 10:52:55 -0500

    "commit"

---
+++ src/client/index.js
@@ -0,0 +1,5 @@
+import { checkForName } from './js/nameChecker'
+import { handleSubmit } from './js/formHandler'
+
+
+alert("I EXIST")
---
+++ src/index.js
@@ -0,0 +1,12 @@
+var path = require('path')
+const express = require('express')
+
+const app = express()
+
+app.use(express.static('dist'))
+
+console.log(__dirname)
+
+app.get('/', function (req, res) {
+    res.sendFile('dist/index.html')
+})
